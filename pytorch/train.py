42e6ab44766b604b3f4ada7c13265d38c67f12c1
sh <sh@sh.co>
2024-06-08 06:47:20 +0300

    generate svg and obj files

--- pytorch/train.py
+++ pytorch/train.py
@@ -23,6 +23,10 @@ from options import parse_args
 from datasets.floorplan_dataset import FloorplanDataset
 from IP import reconstructFloorplan
 
+# Import FloorPlan from floorplan.py
+from rendering.floorplan import FloorPlan
+
+
 # Function to create an SVG element
 def create_svg_element(tag, attrib):
     element = ET.Element(tag, attrib)
@@ -186,8 +190,9 @@ def testOneEpoch(options, model, dataset, device):
                 room_heatmaps = torch.nn.functional.softmax(room_pred[batchIndex], dim=-1).detach().cpu().numpy()
                 print("Reconstructing floorplan for batch {}, image {}".format(sampleIndex, batchIndex))
                 output_prefix = options.test_dir + '/' + str(batchIndex) + '_'
+                floorplan_txt_path = options.test_dir + '/' + str(batchIndex) + '_floorplan.txt'
                 reconstructFloorplan(corner_heatmaps[:, :, :NUM_WALL_CORNERS], corner_heatmaps[:, :, NUM_WALL_CORNERS:NUM_WALL_CORNERS + 4], corner_heatmaps[:, :, -4:], icon_heatmaps, room_heatmaps, output_prefix=output_prefix, densityImage=None, gt_dict=None, gt=False, gap=-1, distanceThreshold=-1, lengthThreshold=-1, debug_prefix='test', heatmapValueThresholdWall=None, heatmapValueThresholdDoor=None, heatmapValueThresholdIcon=None, enableAugmentation=True)
-                
+
                 # Convert floorplan to SVG
                 floorplan_txt_path = output_prefix + 'floorplan.txt'
                 if os.path.exists(floorplan_txt_path):
@@ -199,6 +204,12 @@ def testOneEpoch(options, model, dataset, device):
                         f.write(svg_data)
                     print(f"Saved SVG to {svg_output_path}")
 
+                # Convert floorplan.txt to floorplan.obj
+                floorplan = FloorPlan(floorplan_txt_path)
+                obj_path = options.test_dir + '/' + str(batchIndex) + '_floorplan.obj'
+                floorplan.to_obj(obj_path)
+                print("Converted {} to {}".format(floorplan_txt_path, obj_path))
+                
                 continue
             if options.visualizeMode == 'debug':
                 exit(1)
